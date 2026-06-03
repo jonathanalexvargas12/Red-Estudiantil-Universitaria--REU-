68461226cada7efd02276d577bb57e93283f180e
Jonathan Vargas <jonathanalexvargas@gmail.com>
2026-06-03 17:53:40 -0400

    Implementación de recuperación de contraseña Mail Trap en proceso

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ const mariadb = require('mariadb');
 const pool = mariadb.createPool({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'babaire2026*',
+    password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'reu_db',
     port: parseInt(process.env.DB_PORT) || 3306,
     connectionLimit: 5
